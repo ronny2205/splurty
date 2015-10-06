73a7b48c72ce8e5212f8bc231d2ecbdf2fff0b8f
Ronny <ronny2205@gmail.com>
2015-10-06 17:38:09 +0000

    building a controller test

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class QuotesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "quote show page" do
+    quote = Quote.create(:author => 'Herold Pinter', :saying => 'Shall I put it around my neck?')
+  	get :show, :id => quote.id
+  	assert_response :success
+  end
+
+  test "quote show page, not found" do
+    get :show, :id => 'MAZE'
+    assert_response :not_found
+  end
 end
