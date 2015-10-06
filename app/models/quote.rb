11d5916b95bc8a1f938b228ee3067928ad408c84
Ronny <ronny2205@gmail.com>
2015-10-06 17:12:03 +0000

    building a model test

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,13 @@
 class Quote < ActiveRecord::Base
 	validates :saying, :presence => true, :length => { :maximum => 140, :minimum => 3 }
 	validates :author, :presence => true, :length => { :maximum => 50, :minimum => 3 }
+
+	def unique_tag
+		initial = self.author.split(" ").collect do |name|
+    		name[0]
+    	end
+
+    	initial.join + "#" + self.id.to_s
+  	end
+
 end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  
+  test "unique_tag" do 
+    quote = Quote.create(:author => 'Herold Pinter', :saying => 'Shall I put it around my neck?')
+	expected = 'HP#' + quote.id.to_s 
+	actual = quote.unique_tag
+	assert_equal expected, actual   
   end
+
 end
